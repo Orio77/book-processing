1d1f08c29d4f79f35d8f8319fb1dd36ed101cb92
Maciej Lisak <maciej.lisak@capgemini.com>
2026-03-03 15:09:58 +0100

    2.15 CORS Global Config Introduced

--- src/main/java/com/orio/book_processing/book_management/controllers/PDFController.java
+++ src/main/java/com/orio/book_processing/book_management/controllers/PDFController.java
@@ -3,7 +3,6 @@ package com.orio.book_processing.book_management.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -35,7 +34,6 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/pdf")
-@CrossOrigin(origins = { "http://localhost:5173", "http://localhost:5174", "http://localhost:5175" })
 @RequiredArgsConstructor
 public class PDFController {
 
--- src/main/java/com/orio/book_processing/processing/chapter/controllers/ProcessingController.java
+++ src/main/java/com/orio/book_processing/processing/chapter/controllers/ProcessingController.java
@@ -3,7 +3,6 @@ package com.orio.book_processing.processing.chapter.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +22,6 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/pdf/process")
-@CrossOrigin(origins = { "http://localhost:5173", "http://localhost:5174", "http://localhost:5175" })
 public class ProcessingController {
 
     private final ChapterSummaryWorkflow chapterSummaryWorkflow;
