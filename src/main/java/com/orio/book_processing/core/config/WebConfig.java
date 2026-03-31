33ce36f18250c0115d4890e30eee85231ca8bc77
Maciej Lisak <maciejll@wp.pl>
2026-03-31 10:32:49 +0200

    5.92 WIP Deployment Config Setup

--- src/main/java/com/orio/book_processing/core/config/WebConfig.java
+++ src/main/java/com/orio/book_processing/core/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.orio.book_processing.core.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,14 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
                 .allowedHeaders("*")
-                .allowedOrigins(
-                        "http://localhost:5173",
-                        "http://localhost:5174",
-                        "http://localhost:5175");
+                .allowedOriginPatterns(allowedOrigins);
     }
 }
--- src/main/java/com/orio/book_processing/core/config/WebSocketConfig.java
+++ src/main/java/com/orio/book_processing/core/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.orio.book_processing.core.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,11 +11,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5173",
-                "http://localhost:5174",
-                "http://localhost:5175").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns(allowedOrigins).withSockJS();
     }
 
     @Override
