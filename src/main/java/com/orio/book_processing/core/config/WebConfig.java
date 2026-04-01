8216612337ab002c498fe81484653b6399d715dd
Maciej Lisak <maciejll@wp.pl>
2026-04-01 16:23:35 +0200

    5.945 WIP Deployment Config Setup

--- src/main/java/com/orio/book_processing/core/config/WebConfig.java
+++ src/main/java/com/orio/book_processing/core/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.orio.book_processing.core.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,12 +17,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowedOriginPatterns(allowedOrigins);
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/dist/")
-                .resourceChain(true);
     }
 }
