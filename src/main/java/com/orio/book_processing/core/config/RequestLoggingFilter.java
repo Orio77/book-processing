0c662d6cc79b753c2a1857f09b7c57b67c6831b2
Maciej Lisak <maciejll.lisak@gmail.com>
2026-04-09 16:56:58 +0200

    6.9 Controller Global Request Logging

---
+++ src/main/java/com/orio/book_processing/core/config/RequestLoggingFilter.java
@@ -0,0 +1,26 @@
+package com.orio.book_processing.core.config;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class RequestLoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        filterChain.doFilter(request, response);
+        log.info("Request received {} {} -> {}", request.getMethod(), request.getRequestURI(), response.getStatus());
+    }
+
+}
